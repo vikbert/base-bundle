84ea0600f4793886b50ce4f2d5970db38f253d80
Jan Mollowitz <jan.mollowitz@wirkaufens.de>
2015-05-07 17:12:22 +0200

    Passing fixed type(s) to the param conversion to prevent autoconversion of specified params.

--- Lib/RequestParam.php
+++ Lib/RequestParam.php
@@ -17,11 +17,20 @@ class RequestParam
 {
     /**
      * @param mixed $variable
+     * @param mixed $fixedType
      *
      * @return mixed
      */
-    public static function convertType($variable)
+    public static function convertType($variable, $fixedType = null)
     {
+        if (is_string($fixedType) && !is_array($variable)) {
+            if ($fixedType == 'bool' && $variable === 'false') {
+                return false;
+            }
+            settype($variable, $fixedType);
+            return $variable;
+        }
+
         if (is_numeric($variable)) {
             $float = floatval($variable);
             $int = intval($variable);
@@ -35,11 +44,14 @@ class RequestParam
         // Convert the values recursive if it's an array
         if (is_array($variable)) {
             foreach ($variable as $key => $value) {
-                $variable[$key] = self::convertType($value);
+                if (is_array($fixedType) && isset($fixedType[$key])) {
+                    $variable[$key] = self::convertType($value, $fixedType[$key]);
+                } else {
+                    $variable[$key] = self::convertType($value);
+                }
             }
         }
 
         return $variable;
     }
 }
-
--- Tests/Lib/RequestParamTest.php
+++ Tests/Lib/RequestParamTest.php
@@ -20,10 +20,11 @@ class RequestParamTest extends \PHPUnit_Framework_TestCase
     /**
      * Data provider for testConvertType
      *
-     * @return array
+     * @return array (expected, variable, fixedType)
      */
-    public function dataConvertType()
+    public function validConvertTypeProvider()
     {
+        $stdClassObj = new \stdClass();
         return array(
             array(12, 12),
             array(12, '12'),
@@ -41,7 +42,56 @@ class RequestParamTest extends \PHPUnit_Framework_TestCase
             array(array('key' => 12), array('key' => '12')),
             array(array('key' => 12.5), array('key' => '12.5')),
             array(array('key' => 12.5), array('key' => 12.5)),
-            array(new \stdClass(), new \stdClass()),
+            array($stdClassObj, $stdClassObj),
+            array(12, 12, 'int'),
+            array(12, '12', 'int'),
+            array(12, '12', 'int'),
+            array(true, 'true', 'bool'),
+            array(true, 1, 'bool'),
+            array(true, 2, 'bool'),
+            array(true, true, 'bool'),
+            array(false, 'false', 'bool'),
+            array(false, '0', 'bool'),
+            array(0, '0', 'int'),
+            array(false, null, 'bool'),
+            array(false, false, 'bool'),
+            array(0.0, 'abc', 'float'),
+            array('12', '12', 'string'),
+            array('12.5', '12.5', 'string'),
+            array('12.5', 12.5, 'string'),
+            array(array(12), array(12), 'int'),
+            array(array('12'), array(12), array('string')),
+            array(array(12.0), array(12), array('float')),
+            array(array('12.5'), array('12.5'), array('string')),
+            array(array(12), array('12.5'), array('int')),
+            array(array(0), array('0.5'), array('int')),
+            array(array('12.5'), array(12.5), array('string')),
+            array(array('1.5' => '12'), array('1.5' => '12'), array('1.5' => 'string')),
+            array(array('1.5' => 12.5), array('1.5' => '12.5'), array('1.5'=> 'float')),
+            array(array('1.5' => 12.5), array('1.5' => 12.5), array('1.5' => 'float')),
+            array(array('key' => 12), array('key' => 12), 'float'),
+            array(array('key' => 12), array('key' => 12), array('float')),
+            array(array('key' => 12), array('key' => 12), array('key' => 'int')),
+            array(array('key' => 12), array('key' => '12'), array('key' => 'int')),
+            array(array('key' => '12'), array('key' => '12'), array('key' => 'string')),
+            array(array('key' => '12.5'), array('key' => '12.5'), array('key' => 'string')),
+            array(array('key' => 12.5), array('key' => '12.5'), array('key' => 'float')),
+            array(array('key' => true), array('key' => 'true'), array('key' => 'bool')),
+            array(array('key' => false), array('key' => 'false'), array('key' => 'bool')),
+            array(array('key' => false), array('key' => '0'), array('key' => 'bool')),
+            array(array('key' => 0), array('key' => '0'), array('key' => 'int')),
+            array(array('nested' => array('key' => 12)), array('nested' => array('key' => '12')), array('nested' => array('key' => 'int'))),
+            array(array('nested' => array('key' => 12.0)), array('nested' => array('key' => '12')), array('nested' => array('key' => 'float'))),
+            array(
+                array('nested' => array('key' => 12.0, 'nonFixed' => 12)),
+                array('nested' => array('key' => '12', 'nonFixed' => '12')),
+                array('nested' => array('key' => 'float'))
+            ),
+            array(
+                array('nested' => array('key' => 12, 'noFixedType' => 12)),
+                array('nested' => array('key' => '12', 'noFixedType' => '12')),
+                array('wontBeApplied' => array('key' => 'float'))
+            ),
         );
     }
 
@@ -50,11 +100,13 @@ class RequestParamTest extends \PHPUnit_Framework_TestCase
      *
      * @param mixed $expected
      * @param mixed $variable
+     * @param mixed $fixedType
      *
-     * @dataProvider dataConvertType
+     * @dataProvider validConvertTypeProvider
      */
-    public function testConvertType($expected, $variable)
+    public function testConvertType($expected, $variable, $fixedType = null)
     {
-        $this->assertEquals($expected, RequestParam::convertType($variable));
+        $result = RequestParam::convertType($variable, $fixedType);
+        $this->assertSame($expected, $result);
     }
 }
